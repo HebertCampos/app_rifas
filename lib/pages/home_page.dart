6901ff2bbb54f82ba7f3c50d838315bd3becba07
Hebert <hebertcdev@gmail.com>
2024-08-29 20:48:39 -0300

    popup de visualizacao de pagamento

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:app_rifas/models/numeros_model.dart';
 import 'package:app_rifas/services/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class HomePage extends StatefulWidget {
@@ -86,7 +87,7 @@ class _HomePageState extends State<HomePage> {
                           itemBuilder: (context, index) {
                             final numero = numeros[index];
                             return GestureDetector(
-                              onTap: () => _showInscricaoForm(numero.id),
+                              onTap: () => showPopup(numero.id),
                               child: Container(
                                 margin: const EdgeInsets.all(2),
                                 decoration: BoxDecoration(
@@ -118,6 +119,82 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  void showPopup(int id) async {
+    final comprado = await apiService.getComprados(id);
+    if (comprado != null) {
+      // ignore: use_build_context_synchronously
+      showDialog(
+        context: context,
+        builder: (context) {
+          return SingleChildScrollView(
+            child: AlertDialog(
+              title: Text(
+                "${comprado.nome.toUpperCase()} é o dono do número ${comprado.id}!\nEscolha outro 😅😅! \nBoa sorte...",
+              ),
+              content: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Você pode realizar o pagamente via QR Code ou coipando a chave PIX Copia e Cola',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 200,
+                    child: Image.asset('assets/pix.png'),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Chave PIX',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 242, 92, 132),
+                    ),
+                    onPressed: () {
+                      Clipboard.setData(
+                        const ClipboardData(
+                            text:
+                                '00020126870014BR.GOV.BCB.PIX013664af769b-5063-4c60-b5b0-01eeed2227b80225Rifa da fralda Maria Ísis520400005303986540550.005802BR5925Hebert Douglas da Silva C6009SAO PAULO62140510toOHuBni7I6304237B'),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('PIX Copia e Cola copiado!'),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Copiar chave PIX!',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text(
+                    "Fechar",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    } else {
+      _showInscricaoForm(id);
+    }
+  }
+
   void _showInscricaoForm(int id) {
     final nomeController = TextEditingController();
     final telefoneController = TextEditingController();
@@ -130,106 +207,107 @@ class _HomePageState extends State<HomePage> {
     );
 
     showDialog(
-        context: context,
-        builder: (context) {
-          return SingleChildScrollView(
-            child: AlertDialog(
-              backgroundColor: const Color.fromARGB(255, 255, 243, 243),
-              title: Text('Pegando o número $id 👏👏👏!'),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(top: 20, bottom: 20),
-                    child: const Text(
-                      '* Após realizar a escolha do seu número, volte no seu número escolhido e pegue a chave pix!\n* Envie o comprovante para a mamãe ou o papai!',
-                    ),
-                  ),
-                  TextField(
-                    controller: nomeController,
-                    decoration: const InputDecoration(
-                      labelText: 'Seu Nome',
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 2,
-                          color: Color.fromARGB(255, 242, 92, 132),
-                        ),
-                      ),
-                    ),
-                    cursorColor: const Color.fromARGB(255, 242, 92, 132),
-                    style: const TextStyle(
-                      color: Color.fromARGB(255, 242, 92, 132),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: telefoneController,
-                    decoration: const InputDecoration(
-                      labelText: 'Seu Telefone',
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 2,
-                          color: Color.fromARGB(255, 242, 92, 132),
-                        ),
-                      ),
-                    ),
-                    cursorColor: const Color.fromARGB(255, 242, 92, 132),
-                    style: const TextStyle(
-                        color: Color.fromARGB(255, 242, 92, 132)),
-                    inputFormatters: [maskFormatter],
-                  ),
-                ],
-              ),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    if (nomeController.text.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Por favor, insira seu nome!"),
-                        ),
-                      );
-                      return;
-                    }
-                    if (telefoneController.text.isEmpty ||
-                        !RegExp(r'^\(\d{2}\) \d{5}-\d{4}$')
-                            .hasMatch(telefoneController.text)) {
-                      const SnackBar(
-                          content: Text("Por favor, insira seu telefone!"));
-                      return;
-                    }
-
-                    apiService
-                        .inscrever(
-                            nomeController.text, telefoneController.text, id)
-                        .then((_) {
-                      setState(() {
-                        futureNumeros = apiService.getNumeros();
-                      });
-                      Navigator.of(context).pop();
-                    });
-                  },
+      context: context,
+      builder: (context) {
+        return SingleChildScrollView(
+          child: AlertDialog(
+            backgroundColor: const Color.fromARGB(255, 255, 243, 243),
+            title: Text('Pegando o número $id 👏👏👏!'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 20, bottom: 20),
                   child: const Text(
-                    'Pegar',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 189, 64, 87),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
+                    '* Após realizar a escolha do seu número, volte no seu número escolhido e pegue a chave pix!\n* Envie o comprovante para a mamãe ou o papai!',
                   ),
                 ),
-                TextButton(
-                  child: const Text(
-                    "Cancelar",
-                    style: TextStyle(color: Colors.redAccent),
+                TextField(
+                  controller: nomeController,
+                  decoration: const InputDecoration(
+                    labelText: 'Seu Nome',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Color.fromARGB(255, 242, 92, 132),
+                      ),
+                    ),
                   ),
-                  onPressed: () => Navigator.of(context).pop(),
+                  cursorColor: const Color.fromARGB(255, 242, 92, 132),
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 242, 92, 132),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: telefoneController,
+                  decoration: const InputDecoration(
+                    labelText: 'Seu Telefone',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Color.fromARGB(255, 242, 92, 132),
+                      ),
+                    ),
+                  ),
+                  cursorColor: const Color.fromARGB(255, 242, 92, 132),
+                  style:
+                      const TextStyle(color: Color.fromARGB(255, 242, 92, 132)),
+                  inputFormatters: [maskFormatter],
                 ),
               ],
             ),
-          );
-        });
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  if (nomeController.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Por favor, insira seu nome!"),
+                      ),
+                    );
+                    return;
+                  }
+                  if (telefoneController.text.isEmpty ||
+                      !RegExp(r'^\(\d{2}\) \d{5}-\d{4}$')
+                          .hasMatch(telefoneController.text)) {
+                    const SnackBar(
+                        content: Text("Por favor, insira seu telefone!"));
+                    return;
+                  }
+
+                  apiService
+                      .inscrever(
+                          nomeController.text, telefoneController.text, id)
+                      .then((_) {
+                    setState(() {
+                      futureNumeros = apiService.getNumeros();
+                    });
+                    Navigator.of(context).pop();
+                  });
+                },
+                child: const Text(
+                  'Pegar',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 189, 64, 87),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              TextButton(
+                child: const Text(
+                  "Cancelar",
+                  style: TextStyle(color: Colors.redAccent),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
