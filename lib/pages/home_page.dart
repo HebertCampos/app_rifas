9860d31f9e56e6c82799ecce83761866744ac365
Hebert <hebertcdev@gmail.com>
2024-08-28 22:38:16 -0300

    criando a lista de numeros a serem pegos

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:app_rifas/models/numeros_model.dart';
 import 'package:app_rifas/services/api_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,6 +17,19 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     futureNumeros = apiService.getNumeros();
+  }
+
+  Color getColor(int marcado) {
+    switch (marcado) {
+      case 0:
+        return const Color.fromARGB(255, 242, 92, 132); //LIVRE;
+      case 1:
+        return const Color.fromARGB(255, 12, 77, 89); //CONFIRMADO
+      case 2:
+        return const Color.fromARGB(255, 189, 64, 87); //ESPERANDO CONFIRMACAO
+      default:
+        return Colors.white;
+    }
   }
 
   @override
@@ -37,6 +49,60 @@ class _HomePageState extends State<HomePage> {
                 height: 250,
                 alignment: Alignment.topCenter,
                 child: Image.asset('assets/img_rifa_topo.png'),
+              ),
+              Expanded(
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    int crossAxisCount = constraints.maxWidth > 1020 ? 25 : 10;
+                    return FutureBuilder<List<Numeros>>(
+                      future: futureNumeros,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Center(
+                            child: Text("Erro: ${snapshot.error}"),
+                          );
+                        }
+                        final numeros = snapshot.data!;
+                        return GridView.builder(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: crossAxisCount,
+                            childAspectRatio: 1,
+                          ),
+                          itemCount: numeros.length,
+                          itemBuilder: (context, index) {
+                            final numero = numeros[index];
+                            return GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                margin: const EdgeInsets.all(2),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: getColor(numero.marcado),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '${numero.id}',
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
             ],
           )
