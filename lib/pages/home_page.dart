6c628dcda4be09fea8f7f8a54ff138a5523e0965
Hebert <hebertcdev@gmail.com>
2024-08-28 21:35:16 -0300

    inicializando a apiService

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:app_rifas/models/numeros_model.dart';
+import 'package:app_rifas/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +10,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<List<Numeros>> futureNumeros;
+  final ApiService apiService = ApiService();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureNumeros = apiService.getNumeros();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
